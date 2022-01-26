9669aa507b3a84876522da4409272d9cbe6525d9
Shawn Charles <62077627+CharlesCreativeContent@users.noreply.github.com>
2022-01-26 02:58:43 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -63,6 +63,6 @@ app.use(flash()); // use connect-flash for flash messages stored in session
 //require('./app/routes.js')(app, passport, db); // load our routes and pass in our app and fully configured passport
 
 // launch ======================================================================
-app.set("port",port)
+app.set("port",port);
 app.listen(port);
 console.log('The magic happens on port ' + port);
